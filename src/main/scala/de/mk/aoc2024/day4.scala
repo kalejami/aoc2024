5d8fad81b6306fcca8f6132500139e5b7458fb8c
Michael Kaleja <michael.kaleja@m-net.de>
2024-12-04 12:22:40 +0100

    dev: reformat

--- src/main/scala/de/mk/aoc2024/day4.scala
+++ src/main/scala/de/mk/aoc2024/day4.scala
@@ -1,12 +1,12 @@
 package de.mk.aoc2024
 
-import cats.effect.IOApp
-import cats.effect.{ExitCode, IO}
+import cats.effect.{ExitCode, IO, IOApp}
 import cats.syntax.all._
-import cats.data.Op
+
+import scala.annotation.tailrec
 
 object day4 extends IOApp {
-  val xmas = "XMAS".r
+  private val xmas = "XMAS".r
   def run(args: List[String]): IO[ExitCode] =
     Util
       .getInput("input4.txt")
@@ -21,6 +21,7 @@ object day4 extends IOApp {
 
         val horizontal = count(lines)
 
+        @tailrec
         def vloop(index: Int, vLines: List[String]): List[String] =
           if (index >= lines.head.length()) vLines.reverse
           else {
@@ -68,7 +69,7 @@ object day4 extends IOApp {
 
         val diagonal = count(diags)
 
-        def a1 = horizontal + vertikal + diagonal
+        def a1: Int = horizontal + vertikal + diagonal
 
         val linesWithIndex = lines.zipWithIndex
 
@@ -79,9 +80,10 @@ object day4 extends IOApp {
         val maxY = lines.size
         val maxX = lines.head.length()
 
-        def z(x: Int, y: Int, count: Int): Int =
+        @tailrec
+        def a2Loop(x: Int, y: Int, count: Int): Int =
           if (y >= maxY) count
-          else if (x >= maxX) z(0, y + 1, count)
+          else if (x >= maxX) a2Loop(0, y + 1, count)
           else {
             getChar(x, y) match {
               case Some(c) =>
@@ -97,16 +99,16 @@ object day4 extends IOApp {
                         ul == 'M' && dr == 'S' || ul == 'S' && dr == 'M'
                       val urdlIsFine =
                         ur == 'M' && dl == 'S' || ur == 'S' && dl == 'M'
-                      if (uldrIsfine && urdlIsFine) z(x + 1, y, count + 1)
-                      else z(x + 1, y, count)
-                    case None => z(x + 1, y, count)
+                      if (uldrIsfine && urdlIsFine) a2Loop(x + 1, y, count + 1)
+                      else a2Loop(x + 1, y, count)
+                    case None => a2Loop(x + 1, y, count)
                   }
-                } else z(x + 1, y, count)
+                } else a2Loop(x + 1, y, count)
               case None => count
             }
           }
 
-        def a2 = z(0, 0, 0)
+        def a2: Int = a2Loop(0, 0, 0)
 
         IO.println(a2)
       }
