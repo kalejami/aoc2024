cd5ffe682dc9e0dc79d2ae4f4ca8bd125f973234
kalejami <kalejami@github.com>
2024-12-11 07:22:46 +0100

    day11

--- src/main/scala/de/mk/aoc2024/day11.scala
+++ src/main/scala/de/mk/aoc2024/day11.scala
@@ -13,33 +13,42 @@ object day11 extends IOApp {
 
       val nums = line.split(" ").toVector
 
+      def blink(n: String): Vector[String] =
+        if (n == "0") Vector("1")
+        else if (n.length % 2 == 0) {
+          val (x1, x2) = n.splitAt(n.length / 2)
+          Vector(x1, x2.toLong.toString)
+        } else Vector((n.toLong * 2024).toString)
+
+      def blinkS(n: String) = blink(n).mkString(" ")
+
       @tailrec
       def loop(index: Int, limit: Int, acc: Vector[String]): Vector[String] = {
         if (index > limit) acc
         else {
-          val newAcc = acc.flatMap { n =>
-            if (n == "0") Vector("1")
-            else if (n.length % 2 == 0) {
-              val (x1, x2) = n.splitAt(n.length / 2)
-              Vector(x1, x2.toLong.toString)
-            } else Vector((n.toLong * 2024).toString)
-          }
+          val newAcc = acc.flatMap { blink }
           loop(index + 1, limit, newAcc)
         }
       }
 
+      @tailrec
+      def loop2(index: Int, limit: Int, acc: String): String = {
+        if (index > limit) acc
+        else {
+          val newAcc = acc.split(" ").map(blinkS).mkString(" ")
+          loop2(index + 1, limit, newAcc)
+        }
+      }
 
       def a1: IO[Long] = {
-        val parts = nums.parTraverse( n =>
-          IO(loop(0, 24, Vector(n))).map(_.size.toLong)
-        )
+        val parts =
+          nums.parTraverse(n => IO(loop2(0, 24, n)).map(_.count(_ == ' ').toLong + 1))
         parts.map(_.sum)
       }
 
       def a2: IO[Long] = {
-        val parts = nums.parTraverse( n =>
-          IO(loop(0, 74, Vector(n))).map(_.size.toLong)
-        )
+        val parts =
+          nums.parTraverse(n => IO(loop2(0, 74, n)).map(_.count(_ == ' ').toLong + 1))
         parts.map(_.sum)
       }
 
